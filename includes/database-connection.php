f6dd9248c6ad6cf3078229f2c70eae78d672837e
pmajdalani <90423821+pmajdalani@users.noreply.github.com>
2025-04-16 12:14:31 -0400

    info for database

--- includes/database-connection.php
+++ includes/database-connection.php
@@ -1,13 +1,13 @@
 <?php
 
 $type     = 'mysql';                             // Type of database
-$server   = 'yourIPAddress';                    // Server the database is on
-$db       = 'yourcPanelUsername_horoscope';    // Name of the database
+$server   = '192.185.2.183';                    // Server the database is on
+$db       = 'pmajdala_horoscope';    // Name of the database
 $port     = '3306';                           // Port is usually 3306 in Hostgator
 $charset  = 'utf8mb4';                       // UTF-8 encoding using 4 bytes of data per char
 
-$username = 'yourcPanelUsername_user';     // Enter YOUR cPanel username and user here
-$password = 'yourUserPassword';           // Enter YOUR user password here                                     
+$username = 'pmajdala_horoscopeAdmin';     // Enter YOUR cPanel username and user here
+$password = 'dragon8542!!';           // Enter YOUR user password here                                     
 
 
 
